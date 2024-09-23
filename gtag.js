01104e38b1cba4a1b2a8e2a485a7888cece932c5
MatthijsOord <118860044+MatthijsOord@users.noreply.github.com>
2024-09-23 10:51:13 +0200

    Create gtag.js
    added gtag

---
+++ gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-08532F92QJ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-08532F92QJ');
+</script>
