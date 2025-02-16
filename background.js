9098db3eb3957c877b56e15d1098252fc4f5c177
MatthijsOord <118860044+MatthijsOord@users.noreply.github.com>
2025-02-16 10:08:57 +0100

    Shows background stays locked

---
+++ background.js
@@ -0,0 +1,11 @@
+function isMobile() {
+    return /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+
+if (isMobile()) {
+    document.querySelector(".shows-page-background").style.position = "absolute"; // Override fixed
+
+    document.addEventListener("scroll", function () {
+        document.querySelector(".shows-page-background").style.transform = `translateY(${window.scrollY}px)`;
+    });
+}
