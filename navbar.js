d47f65e4127078e9006f92dfe589f85eb3ce5a5c
matthijsoord <matthijscoord@gmail.com>
2025-11-04 17:00:13 +0100

    Small navbar fix and spotify and apple music icon added to footer

--- navbar.js
+++ navbar.js
@@ -17,4 +17,18 @@ document.addEventListener('DOMContentLoaded', () => {
     if (window.scrollY === 0) {
         navBar.classList.add('transparent');
     }
+
+    const repertoireSection = document.getElementById('repertoire');
+    const repertoireLink = document.querySelector('.nav-bar a[href="about#repertoire"]');
+    const aboutLink = document.querySelector('.nav-bar a.active');
+
+    repertoireSection.addEventListener('mouseenter', () => {
+        repertoireLink.classList.add('active');
+        aboutLink.classList.remove('active');
+    });
+
+    repertoireSection.addEventListener('mouseleave', () => {
+        repertoireLink.classList.remove('active');
+        aboutLink.classList.add('active');
+    });
 });
